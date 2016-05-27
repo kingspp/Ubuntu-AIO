0f08b51aa788376d033af2260de631ab0511e630
Prathyush <kingspprathyush@gmail.com>
2016-05-27 16:19:15 +0530

    added flask-httpauth

--- install_tensorflow.sh
+++ install_tensorflow.sh
@@ -3,5 +3,6 @@
 echo "Installing tensorflow"
 sudo apt-get install -y python-pip python-dev
 sudo pip install matplotlib
-sudo pip install flask
+sudo pip install flask --upgrade
+sudo pip install flask-httpauth
 sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.8.0-cp27-none-linux_x86_64.whl
