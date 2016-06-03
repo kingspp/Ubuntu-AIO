d85f70a6250913a109e9ccc2ba47af003f306f16
Prathyush <kingspprathyush@gmail.com>
2016-06-03 14:41:37 +0530

    added scipy

--- install_tensorflow.sh
+++ install_tensorflow.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Installing tensorflow"
-sudo apt-get install -y python-pip python-dev libpng-dev libfreetype6-dev
-sudo pip install matplotlib
+sudo apt-get install -y python-pip python-dev 
+sudo pip install libpng-dev libfreetype6-dev matplotlib
 sudo pip install jupyter
 sudo pip install pandas
-sudo pip install flask --upgrade
-sudo pip install flask-httpauth
+sudo pip install scipy
+sudo pip install flask flask-httpauth --upgrade
 sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.8.0-cp27-none-linux_x86_64.whl
