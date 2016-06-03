4777578fc598de83af5e72c3eccfc3a654e48b67
Prathyush <kingspprathyush@gmail.com>
2016-06-03 13:49:48 +0530

    added pandas

--- install_tensorflow.sh
+++ install_tensorflow.sh
@@ -4,6 +4,7 @@ echo "Installing tensorflow"
 sudo apt-get install -y python-pip python-dev libpng-dev libfreetype6-dev
 sudo pip install matplotlib
 sudo pip install jupyter
+sudo pip install pandas
 sudo pip install flask --upgrade
 sudo pip install flask-httpauth
 sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.8.0-cp27-none-linux_x86_64.whl
