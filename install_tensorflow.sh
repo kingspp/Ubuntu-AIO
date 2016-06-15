0c85d08766db7149295186893e9caa2239ed24e1
Prathyush <kingspprathyush@gmail.com>
2016-06-15 15:15:47 +0530

    added simplejson and jsonpickle

--- install_tensorflow.sh
+++ install_tensorflow.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+$pip=pip
+
 echo "Installing tensorflow"
 sudo apt-get install -y python-pip python-dev 
-sudo pip install libpng-dev libfreetype6-dev matplotlib
-sudo pip install jupyter
-sudo pip install pandas
-sudo pip install scipy
-sudo pip install flask flask-httpauth --upgrade
-sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.8.0-cp27-none-linux_x86_64.whl
+sudo $pip install libpng-dev libfreetype6-dev matplotlib
+sudo $pip install jupyter
+sudo $pip install pandas
+sudo $pip install scipy
+sudo $pip install flask flask-httpauth --upgrade
+sudo $pip install simplejson jsonpickle
+#sudo $pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.8.0-cp27-none-linux_x86_64.whl
