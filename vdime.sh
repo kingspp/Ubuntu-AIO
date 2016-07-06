e1310fc3dc1f75fa9c226126826c3a00a53bfa3b
Prathyush <kingspprathyush@gmail.com>
2016-07-06 22:32:56 +0530

    Create vdime.sh

---
+++ vdime.sh
@@ -0,0 +1,14 @@
+# Install Software Properties
+sudo apt-get install software-properties-common
+
+# Install Git
+sudo apt-get install git
+
+# Install Java 8
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
+# Install maven
+sudo apt-get install maven
+
