69a4d8f2747570850f419736f8628f25febc288b
Prathyush <kingspprathyush@gmail.com>
2016-08-23 17:58:03 +0530

    added cuda version check

--- tensorflow_gpu_setup.sh
+++ tensorflow_gpu_setup.sh
@@ -17,6 +17,9 @@ sudo cp cuda/include/cudnn.h /usr/local/cuda/include
 sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
 sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
 
+# Check Cuda Version:
+cat /usr/local/cuda/include/cudnn.h | grep CUDNN_MAJOR -A 2
+
 # Append Environment variables
 echo 'export CUDA_HOME=/usr/local/cuda' >> ~/.bashrc
 echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/lib64' >> ~/.bashrc
