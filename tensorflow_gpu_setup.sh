c67c57c132a0c62e51a751ced53571b6b2f36acb
Prathyush <kingspprathyush@gmail.com>
2016-08-23 17:31:21 +0530

    Added TF Installation

--- tensorflow_gpu_setup.sh
+++ tensorflow_gpu_setup.sh
@@ -23,3 +23,10 @@ echo 'export CUDA_HOME=/usr/local/cuda' >> ~/.bashrc
 echo 'export CUDA_VISIBLE_DEVICES=0' >> ~/.bashrc
 echo 'export LC_ALL=en_US.UTF-8' >> ~/.bashrc
 echo 'export LANG=en_US.UTF-8' >> ~/.bashrc
+
+# Check Python version:
+# Python 3.4.3+
+
+# Python 3.4 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.10.0rc0-cp34-cp34m-linux_x86_64.whl
+# Python 3.5 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.10.0rc0-cp35-cp35m-linux_x86_64.whl
+sudo pip3 install --upgrade $TF_BINARY_URL
